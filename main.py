05bcaf4e85d5ecc68c1c0159e19ff66de2842f67
David Echelberger <dfechelb@ncsu.edu>
2020-01-14 22:10:42 -0500

    new vibe 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import aws_keys
 import boto3
+from datetime import date
 
 phone_numbers = [
-    "+17044952493", #Tyler
-    "+18284558967", #Mason
-    "+19196021971", #Keith
+    # "+17044952493", #Tyler
+    # "+18284558967", #Mason
+    # "+19196021971", #Keith
     "+18282449366", #David
 ]
 
@@ -22,5 +23,5 @@ for number in phone_numbers:
     # Send sms message.
     response = client.publish(
         PhoneNumber=number,
-        Message="🚨 Market Open 🚨\n\nHead Down 🤡👇\nMoney Up 💰🆙\nNo Questions 🚫❓\n\n📈🚀🚀🚀🌕"
+        Message="🚨 Market Open: " + str(date.today().strftime("%m/%d")) + " 🚨\n\nHead Down 🤡👇\nMoney Up 💰🆙\nNo Questions 🚫❓\n\n📈🚀🚀🚀🌕"
     )
